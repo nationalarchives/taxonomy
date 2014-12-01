9540340e9834c6f1e18f5321c687f05a2162ce96
Jeremie Charlet <Jeremie.Charlet@nationalarchives.gsi.gov.uk>
2014-12-01 16:25:19 +0000

    make CLI jar runnable

--- cat-cli/src/main/java/gov/tna/discovery/taxonomy/cli/CLIRunner.java
+++ cat-cli/src/main/java/gov/tna/discovery/taxonomy/cli/CLIRunner.java
@@ -1,6 +1,8 @@
 package gov.tna.discovery.taxonomy.cli;
 
 import gov.tna.discovery.taxonomy.CLIApplication;
+import gov.tna.discovery.taxonomy.repository.domain.mongo.Category;
+import gov.tna.discovery.taxonomy.repository.mongo.CategoryRepository;
 import gov.tna.discovery.taxonomy.service.Categoriser;
 import gov.tna.discovery.taxonomy.service.TrainingSetService;
 
@@ -18,9 +20,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Component
 public class CLIRunner implements CommandLineRunner {
+
+    private static final String OPTION_TEST_CATEGORISE_ALL = "testCategoriseAll";
+
+    private static final String OPTION_TEST_CATEGORISE_SINGLE = "testCategoriseSingle";
+
+    private static final String OPTION_INDEX = "index";
+
+    private static final String OPTION_UPDATE = "update";
 
     private static final Logger logger = LoggerFactory.getLogger(CLIApplication.class);
 
@@ -30,6 +41,9 @@ public class CLIRunner implements CommandLineRunner {
     @Autowired
     TrainingSetService trainingSetService;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
     public void run(String... args) throws IOException, ParseException, org.apache.commons.cli.ParseException {
 
 	logger.info("Start cat CLI Runner.");
@@ -38,10 +52,10 @@ public class CLIRunner implements CommandLineRunner {
 	Options options = new Options();
 
 	// add t option
-	options.addOption("create", false, "create training set");
-	options.addOption("index", false, "index training set");
-	options.addOption("testCategoriseSingle", false, "test the categorisation of one IAView Solr element");
-	options.addOption("testCategoriseAll", false, "test the categorisation of the whole IAView Solr index");
+	options.addOption(OPTION_UPDATE, false, "update (create if not existing) training set");
+	options.addOption(OPTION_INDEX, false, "index training set");
+	options.addOption(OPTION_TEST_CATEGORISE_SINGLE, false, "test the categorisation of one IAView Solr element");
+	options.addOption(OPTION_TEST_CATEGORISE_ALL, false, "test the categorisation of the whole IAView Solr index");
 
 	CommandLineParser parser = new BasicParser();
 	CommandLine cmd = parser.parse(options, args);
@@ -52,22 +66,28 @@ public class CLIRunner implements CommandLineRunner {
 
 	}
 
-	if (cmd.hasOption("create training set")) {
-	    logger.info("testCategoriseAll");
-	    trainingSetService.createTrainingSet(null);
+	if (cmd.hasOption(OPTION_UPDATE)) {
+	    logger.info("update (create if not existing) training set");
+	    String categoryCiaid = cmd.getOptionValue(OPTION_UPDATE);
+	    if (StringUtils.isEmpty(categoryCiaid)) {
+		trainingSetService.createTrainingSet(null);
+	    } else {
+		Category category = categoryRepository.findByCiaid(categoryCiaid);
+		trainingSetService.updateTrainingSetForCategory(category, null);
+	    }
 	}
 
-	if (cmd.hasOption("index")) {
+	if (cmd.hasOption(OPTION_INDEX)) {
 	    logger.info("index training set");
 	    trainingSetService.indexTrainingSet();
 	}
 
-	if (cmd.hasOption("testCategoriseSingle")) {
+	if (cmd.hasOption(OPTION_TEST_CATEGORISE_SINGLE)) {
 	    logger.info("testCategoriseSingle on document: {} ", "");
 	    categoriser.categoriseIAViewSolrDocument("CO 273/632/2");
 	}
 
-	if (cmd.hasOption("testCategoriseAll")) {
+	if (cmd.hasOption(OPTION_TEST_CATEGORISE_ALL)) {
 	    logger.info("test the categorisation of the whole IAView Solr index");
 	    categoriser.testCategoriseIAViewSolrIndex();
 	}
---
+++ cat-ws/src/main/java/gov/tna/discovery/taxonomy/config/WebConfig.java
@@ -0,0 +1,57 @@
+package gov.tna.discovery.taxonomy.config;
+
+import java.util.Map.Entry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.WebRequestInterceptor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.DispatcherServletWebRequest;
+import org.springframework.web.servlet.handler.WebRequestHandlerInterceptorAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    public class LogRequestInterceptor implements WebRequestInterceptor {
+	private Logger logger = LoggerFactory.getLogger(LogRequestInterceptor.class);
+
+	@Override
+	public void preHandle(WebRequest pRrequest) throws Exception {
+	    DispatcherServletWebRequest request = (DispatcherServletWebRequest) pRrequest;
+	    logger.info("WS > {}", request.getRequest().getRequestURI());
+	    for (Entry<String, String[]> parameter : request.getParameterMap().entrySet()) {
+		logger.info("Parameter: {} = {}", parameter.getKey(), parameter.getValue());
+	    }
+
+	}
+
+	@Override
+	public void postHandle(WebRequest pRrequest, ModelMap model) throws Exception {
+	    DispatcherServletWebRequest request = (DispatcherServletWebRequest) pRrequest;
+	    logger.info("WS > {}", request.getRequest().getRequestURI());
+	    for (Entry<String, Object> parameter : model.entrySet()) {
+		logger.info("Parameter: {} = {}", parameter.getKey(), parameter.getValue());
+	    }
+	}
+
+	@Override
+	public void afterCompletion(WebRequest request, Exception ex) throws Exception {
+	    // TODO Auto-generated method stub
+
+	}
+
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+	WebRequestInterceptor logRequestInterceptor = new LogRequestInterceptor();
+	registry.addInterceptor(new WebRequestHandlerInterceptorAdapter(logRequestInterceptor));
+    }
+
+}
