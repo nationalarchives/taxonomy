5cf539225f2c153d270a95df388422073dd24ed2
Jeremie Charlet <Jeremie.Charlet@nationalarchives.gsi.gov.uk>
2015-02-19 12:05:17 +0000

    modified scripts for deployment and running

---
+++ scripts/run/load-vars-tooling.sh
@@ -0,0 +1,9 @@
+source /home/jcharlet/apps/taxonomy/scripts/conf/conf-tooling.sh;
+#source /home/jcharlet/_workspace/cat/scripts/run/conf/conf-local.sh;
+cliPackageFolder=$cliPackageFolder
+wsPackageFolder=$wsPackageFolder
+logsFolder=$logsFolder
+iaViewIndexFolder=$iaViewIndexFolder
+profile=$profile
+mongoDbHostName=$mongoDbHostName
+port=$port
