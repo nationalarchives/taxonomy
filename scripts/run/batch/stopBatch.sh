662e126bb4379118746d7f2b6a487371677eb42d
Jeremie Charlet <Jeremie.Charlet@nationalarchives.gsi.gov.uk>
2015-03-20 12:57:00 +0000

    script to run cluster

--- scripts/run/batch/stopBatch.sh
+++ scripts/run/batch/stopBatch.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-ps aux | grep taxonomy | grep batch | awk "{print \$2}"  | xargs kill
+action=
+if [ -n "$1" ]
+then
+	action=$1
+fi
+ps aux | grep taxonomy | grep batch | grep $action | awk "{print \$2}"  | xargs kill
