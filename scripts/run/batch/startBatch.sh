fe0deefbb98db9086cfe55603b02865c9e93d8fd
Jeremie Charlet <Jeremie.Charlet@nationalarchives.gsi.gov.uk>
2015-03-20 12:57:00 +0000

    script to run cluster

--- scripts/run/batch/startBatch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-source ../../conf/conf.sh;
-java -jar -Dspring.profiles.active=${profile} $@ ${batchPackageFolder}/taxonomy-batch-0.0.1-SNAPSHOT.jar &
-tail -f ${logsFolder}/batch/*
