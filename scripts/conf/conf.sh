c51abcbf178264836963fbafe55161f103327a61
Jeremie Charlet <Jeremie.Charlet@nationalarchives.gsi.gov.uk>
2015-04-07 17:19:09 +0100

    POC done on cli

--- scripts/conf/conf.sh
+++ scripts/conf/conf.sh
@@ -2,15 +2,16 @@
 echo LOAD CONF LOCAL
 
 javaBinary=/usr/java/jdk1.8.0_40/bin/java
-server=
+server=local
 
 cliPackageFolder=/home/jcharlet/_workspace/cat/taxonomy-cli/target
 wsPackageFolder=/home/jcharlet/_workspace/cat/taxonomy-ws/target
 
 batchPackageFolder=/home/jcharlet/_workspace/cat/taxonomy-batch/target
 
+appConfFolder=/home/jcharlet/_workspace/cat_configuration
 scriptsFolder=
-tmpFolder=
+tmpFolder=/tmp
 
 logsFolder=/home/jcharlet/_workspace/cat/bin/logs
 iaViewIndexFolder=/mnt/search_indexes/index_20150217
@@ -18,3 +19,4 @@ iaViewIndexFolder=/mnt/search_indexes/index_20150217
 profile=local,queryBased
 mongoDbHostName=localhost
 port=8090
+
