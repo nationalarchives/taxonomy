bf5c64d833b430e019284c96c13af3f782d2d006
Jeremie Charlet <Jeremie.Charlet@nationalarchives.gsi.gov.uk>
2015-04-10 18:10:11 +0100

    externalised config files for taxonomy: removed sensitive data: solr unit test cases and dataset

--- scripts/run/ws/warmIndexReader.sh
+++ scripts/run/ws/warmIndexReader.sh
@@ -6,7 +6,7 @@ counter=1;
 mongo ${mongoDbHostName}:27017/taxonomy --eval 'printjson(db.categories.find({},{qry:1,_id:0}).toArray())' | egrep -v "\[|{|}|\]" | sed s/\"qry\"/{\"categoryQuery\"/g | sed 's/.$/",\"limit\":\"1\"}/g' | grep -v MongoDB | grep -v "connecting to:" | while read -r line; 
 do 
 	echo $counter;
-	curl -s http://localhost:8090/taxonomy/search --data "$line" -H "content-type: application/json" --noproxy localhost; 
+	curl -s http://localhost:${port}/taxonomy/search --data "$line" -H "content-type: application/json" --noproxy localhost; 
 	echo;
 	echo;
 	counter=$((counter+1)); 
